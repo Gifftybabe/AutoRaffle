e90ff935a7d12b8a92106537bc0a76baa6b5f2f6
Gifftybabe <ulokangozi@gmail.com>
2024-12-17 20:00:24 +0100

    updated deploy code

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,9 +5,10 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {
+    function run() external returns (Raffle, HelperConfig) {
        HelperConfig helperConfig = new HelperConfig();
        ( uint256 entranceFee,
         uint256 interval,
@@ -16,6 +17,12 @@ contract DeployRaffle is Script {
         uint64 subscriptionId,
         uint32 callbackGasLimit) = helperConfig.activeNetworkConfig(); // Destructuring the network config objects into it's underlined parameters
         // NetworkConfig config = helperConfig.activeNetworkConfig();
+
+        if (subscriptionId == 0) {
+            // We are going to need to create a subscription!
+            CreateSubscription createSubscription = new CreateSubscription();
+            subscriptionId = createSubscription.createSubscription(vrfCoordinator);
+        }
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -28,6 +35,6 @@ contract DeployRaffle is Script {
         );
         vm.stopBroadcast();
 
-        return raffle;
+        return (raffle, helperConfig);
     }
 }
